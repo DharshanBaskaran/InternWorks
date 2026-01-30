bb23ea22a6b35a2aeb307b85645ada7bca1b6a82
DharshanBaskaran <dharshansb9524@gmail.com>
2026-01-30 12:43:26 +0530

    commiting update
    worked with allure to generate html reports

--- SampleTests/FormFilling/features/environment.py
+++ SampleTests/FormFilling/features/environment.py
@@ -1,22 +1,34 @@
+import allure
 from utils.driver_factory import get_driver
 from utils.logger import logger
 
 def before_all(context):
     logger.info("Test Execution Starts!!..")
+    context.driver = get_driver(kiosk_printing=True)
 
 def before_feature(context, feature):
     logger.info(f"feature starts: {feature.name}")
+    allure.dynamic.feature(feature.name)
 
 def before_scenario(context, scenario):
     logger.info(f"scenario starts: {scenario.name}")
     context.driver = get_driver(kiosk_printing=True)
+    allure.dynamic.title(scenario.name)
 
 def before_step(context, step):
     logger.info(f"step starts: {step.name}")
+    context.allure_step = allure.step(step.name)
+    context.allure_step.__enter__()
 
 def after_step(context, step):
+    if hasattr(context,"allure_step"):
+        context.allure_step.__exit__(None, None, None)
+
     if step.status == "failed":
         logger.error(f"step failed: {step.name}")
+        if hasattr(context,"driver"):
+            screenshot=context.driver.get_screenshot_as_png()
+            allure.attach(screenshot, name=step.name, attachment_type=allure.attachment_type.PNG)
 
 def after_scenario(context, scenario):
     if scenario.status == "failed":
--- SampleTests/FormFilling/features/steps/form_submission001.py
+++ SampleTests/FormFilling/features/steps/form_submission001.py
@@ -1,20 +1,25 @@
+import allure
 from behave import given,when,then
 from pages import FormPage
 from config import variables
 
 @given("user launches the training form successfully")
 def step_launch_application(context):
-    context.driver.get("https://hirschqa.azurewebsites.net/training/")
-    context.form_page = FormPage(context.driver)
+    with allure.step("Launch the training form"):
+        context.driver.get("https://hirschqa.azurewebsites.net/training/")
+        context.form_page = FormPage(context.driver)
 
 @when("user fills all required details")
 def step_fill_form(context):
-    context.form_page.fill_form(variables)
+    with allure.step("fill all required details"):
+        context.form_page.fill_form(variables)
 
 @when("user views the pdf")
 def step_views_pdf(context):
-    pass
+    with allure.step("user viewing the pdf"):
+        pass
 
 @then("form should be submitted and print action should be triggered")
 def step_submit_form(context):
-    assert True
+    with allure.step("Form submission performed successfully"):
+        assert True
